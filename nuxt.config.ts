ad83a6c2828c1bb6d7046591ac3c84048a6618cc
jonatan shoshan <jonatan-bs@live.dk>
2022-12-18 10:49:33 +0100

    Setup tailwind conf
    ..

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,24 @@
 export default defineNuxtConfig({
 	css: ["~/assets/styles/main.scss"],
 	modules: ["@nuxtjs/tailwindcss"],
+	app: {
+		head: {
+			title: "Jonatan Shoshan - Webspecialist",
+			link: [
+				{
+					rel: "preconnect",
+					href: "https://fonts.googleapis.com",
+				},
+				{
+					rel: "preconnect",
+					href: "https://fonts.gstatic.com",
+					crossorigin: "use-credentials",
+				},
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,400&display=swap",
+				},
+			],
+		},
+	},
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,81 @@
+import plugin from "tailwindcss/plugin";
+
 module.exports = {
-	content: [],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				xl: "14rem",
+			},
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1440px",
+			"3xl": "1920px",
+		},
+		colors: {
+			// BLACKS
+			"black-lacquer": "#403E3E",
+
+			// GREYS
+			"grey-cascading-white": "#F6F6F6",
+			"grey-robo-master": "#ADADAD",
+
+			// YELLOWs
+			"yellow-energy-yellow": "#F5D653",
+		},
+		fontFamily: {
+			sans: ["Lato", "sans-serif"],
+		},
+		spacing: {
+			"2xs": "1.6rem",
+			xs: "2rem",
+			sm: "3rem",
+			md: "4rem",
+			lg: "7rem",
+			xl: "20rem",
+			"2xl": "35rem",
+			"3xl": "59rem",
+		},
+
+		fontSize: {
+			base: "2.1rem",
+			md: "2.6rem",
+			lg: "3rem",
+			xl: "4.9rem",
+			"2xl": "7rem",
+		},
+		fontWeights: {
+			light: 300,
+			normal: 400,
+			bold: 700,
+			black: 900,
+		},
+		borderRadius: {
+			none: "0",
+			sm: "1.6rem",
+			lg: "15.5rem",
+		},
+		aspectRatio: {
+			"1": "1",
+			"5/4": "5 / 4",
+		},
 	},
-	plugins: [],
+
+	plugins: [
+		plugin(function ({ addComponents }) {
+			addComponents({
+				".main-grid": {
+					display: "grid",
+					gridTemplateColumns: "repeat(12, 1fr)",
+					columnGap: "1.6rem",
+					rowGap: "1.6rem",
+				},
+			});
+		}),
+	],
 };
